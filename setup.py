b7e87b6b4f9e2bc1d1ed1e90ab29039590a0c14b
hellman <hellman1908@gmail.com>
2014-04-22 21:44:16 +0400

    basic setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+from distutils.core import setup
+
+setup(name='xortool',
+      version='1.0',
+      description='Tool for xor cipher analysis',
+
+      author='hellman',
+      author_email='hellman1908@gmail.com',
+      license="MIT",
+
+      url='https://github.com/hellman/xortool',
+      packages=['xortool'],
+      scripts=["xortool/xortool", "xortool/xortool-xor"]
+      )
