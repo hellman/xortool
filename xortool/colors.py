b2ac7d348cc76a7775fdf98f70e7b6416d64b231
Markus Linnala <markus.linnala@gmail.com>
2019-09-18 12:12:07 +0300

    colors: Add COLORS dict to simplify color usage with format

--- xortool/colors.py
+++ xortool/colors.py
@@ -17,3 +17,17 @@ C_DIV = color(attrs="bold")
 C_KEY = color("red", attrs="bold")
 C_BOLD = color(attrs="bold")
 C_COUNT = color("yellow", attrs="bold")
+
+COLORS = {
+    'C_RESET': C_RESET,
+    'C_FATAL': C_FATAL,
+    'C_WARN': C_WARN,
+    'C_KEYLEN': C_KEYLEN,
+    'C_PROB': C_PROB,
+    'C_BEST_KEYLEN': C_BEST_KEYLEN,
+    'C_BEST_PROB': C_BEST_PROB,
+    'C_DIV': C_DIV,
+    'C_KEY': C_KEY,
+    'C_BOLD': C_BOLD,
+    'C_COUNT': C_COUNT,
+}
