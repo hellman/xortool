00c04bfbd5b7b19d746acab1e6bdad14d4f6cf03
Markus Linnala <markus.linnala@gmail.com>
2019-09-18 12:02:55 +0300

    Use python3 and fix style in coding mark

--- xortool/args.py
+++ xortool/args.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python2
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 from docopt import docopt
 
--- xortool/routine.py
+++ xortool/routine.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python2
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 import os
 import sys
